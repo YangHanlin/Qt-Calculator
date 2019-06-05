a05844757254889bc7eb5b36c4d6bf18ed158ebb
YangHanlin <mattoncis@hotmail.com>
2019-06-05 14:01:16 +0800

    Move the style definitions to QSS files

--- QtCalculator/main.cpp
+++ QtCalculator/main.cpp
@@ -1,8 +1,15 @@
 #include "Calculator.h"
 #include <QtWidgets/QApplication>
+#include <QFile>
+#include <stdexcept>
 
 int main(int argc, char *argv[]) {
 	QApplication application(argc, argv);
+    QFile mainStylesheet("main.qss");
+    if (mainStylesheet.open(QFile::ReadOnly)) {
+        QString mainStylesheetStr = QLatin1String(mainStylesheet.readAll());
+        application.setStyleSheet(mainStylesheetStr);
+    }
 	Calculator calculator;
 	calculator.show();
 	return application.exec();
