b8c491b556b029393c071058a83353f01d2f5f25
YangHanlin <mattoncis@hotmail.com>
2019-06-07 14:55:11 +0800

    Complete mechanism for fetching button styles

--- QtCalculator/main.cpp
+++ QtCalculator/main.cpp
@@ -9,8 +9,14 @@ int main(int argc, char *argv[]) {
 	QApplication application(argc, argv);
 	Calculator calculator;
     CalculatorSettings calculatorSettings;
-    QObject::connect(&calculator, SIGNAL(fetchCaptionStr(QString&)), &calculatorSettings, SLOT(onProgramNameRequested(QString&)));
-    QObject::connect(&calculator, SIGNAL(fetchAboutInfoStr(QString&)), &calculatorSettings, SLOT(onAboutInfoStrRequested(QString&)));
+    QObject::connect(&calculator, SIGNAL(fetchCaptionStr(QString&)),
+                     &calculatorSettings, SLOT(onProgramNameRequested(QString&)));
+    QObject::connect(&calculator, SIGNAL(fetchAboutInfoStr(QString&)),
+                     &calculatorSettings, SLOT(onAboutInfoStrRequested(QString&)));
+    QObject::connect(&calculator, SIGNAL(fetchOriginalButtonStyle(QString&)),
+                     &calculatorSettings, SLOT(onOriginalButtonStyleRequested(QString&)));
+    QObject::connect(&calculator, SIGNAL(fetchPressedButtonStyle(QString&)),
+                     &calculatorSettings, SLOT(onPressedButtonStyleRequested(QString&)));
     calculator.initWidgets();
 	calculator.show();
 	return application.exec();
