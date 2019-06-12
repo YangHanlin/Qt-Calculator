e49b9432ca86121acf4a833c33e26b3fb95e3a8f
YangHanlin <mattoncis@hotmail.com>
2019-06-12 09:40:54 +0800

    Minor changes

--- QtCalculator/CalculatorController.cpp
+++ QtCalculator/CalculatorController.cpp
@@ -9,7 +9,7 @@ void CalculatorController::calculate(QString expression, CalculationResult& res)
         res.result = evalIntegerExpr(validate(neaten(expression)));
         res.status = 0;
     } catch (const CalculationLogicError& e) {
-        res.errorMessage = "Please check your expression: " + e.description();
+        res.errorMessage = "Check your expression: " + e.description();
         res.status = 1;
     } catch (const CalculationRuntimeError& e) {
         res.errorMessage = "Internal error: " + e.description();
